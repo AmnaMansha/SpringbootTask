8f29bd5c11e6928ed04f5ab4859ca646d1862ecb
amna mansha <amnamansha11@gmail.com>
2024-11-10 17:09:11 +0100

    signed commit

--- src/test/java/com/emse/spring/automacorp/AutomacorpApplicationTests.java
+++ src/test/java/com/emse/spring/automacorp/AutomacorpApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AutomacorpApplicationTests {
 //test 
-// signed commit 
+// signed commit try
 	@Test
 	void contextLoads() {
 	}
